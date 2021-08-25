5439a6cd04b05820fccd3f0570d2f93726a20ad6
Maximiliano <maximilianodacruz@gmail.com>
2021-08-25 08:51:26 -0300

    chore: seleção de ambiente

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,15 @@
 export default class Utils {
 
   static getBaseUrl() {
+    let env = __ENV.env;
+
+    switch (env) {
+      case "PRD":
         return 'https://serverest.dev/';
+        break;
+      default:
+        console.error("Url base not defined.");
+        break;
+    }
   }
 }
